3c921212615b6999c024e9d60551c8fd7650e7ea
Anstasia <lapteva_02@list.ru>
2024-10-09 11:44:01 +0300

    Исправила замечания по стилю кода еще раз (6)

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,11 +1,11 @@
 package service;
 //Это класс для создания других классов???
 public class Managers {
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager(new InMemoryHistoryManager());
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
